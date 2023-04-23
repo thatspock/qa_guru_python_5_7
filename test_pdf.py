778d595e29a1779992c14c83fd43bd765d23439c
Some Name <thatspock@gmail.com>
2023-04-23 20:58:24 +0700

    add all asserts and universal paths

--- test_pdf.py
+++ test_pdf.py
@@ -1,10 +1,24 @@
 from pypdf import PdfReader
+import os.path
+
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-reader = PdfReader("resources/docs-pytest-org-en-latest.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(page)
-print(number_of_pages)
-print(text)
+def test_pdf():
+    download_folder = 'resources'
+    pdf_file_path = os.path.join(download_folder, 'docs-pytest-org-en-latest.pdf')
+
+    assert os.path.exists(pdf_file_path), f'Pdf file {pdf_file_path} was not found'
+
+    reader = PdfReader(pdf_file_path)
+
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+    text = page.extract_text()
+
+    assert number_of_pages == 412
+    assert page is not None, 'Page is empty'
+    assert text is not None and len(text.strip()) > 0, 'Text is empty'
+
+    print(f'\n1: {number_of_pages}')
+    print(f'2: {page}')
+    print(f'3: {text}')
--- test_xls.py
+++ test_xls.py
@@ -1,14 +1,32 @@
 import xlrd
+import os.path
+
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-book = xlrd.open_workbook('resources/file_example_XLS_10.xls')
-print(f'Количество листов {book.nsheets}')
-print(f'Имена листов {book.sheet_names()}')
-sheet = book.sheet_by_index(0)
-print(f'Количество столбцов {sheet.ncols}')
-print(f'Количество строк {sheet.nrows}')
-print(f'Пересечение строки 9 и столбца 1 = {sheet.cell_value(rowx=0, colx=1)}')
-# печать всех строк по очереди
-for rx in range(sheet.nrows):
-    print(sheet.row(rx))
+def test_xls():
+    download_folder = 'resources'
+    xls_file_path = os.path.join(download_folder, 'file_example_XLS_10.xls')
+
+    assert os.path.exists(xls_file_path), f'XLS file {xls_file_path} was not found'
+
+    book = xlrd.open_workbook(xls_file_path)
+
+    assert book.nsheets == 1
+    assert book.sheet_names() == ['Sheet1']
+
+    print(f'\nКоличество листов {book.nsheets}')
+    print(f'Имена листов {book.sheet_names()}')
+
+    sheet = book.sheet_by_index(0)
+
+    assert sheet.ncols == 8
+    assert sheet.nrows == 10
+    assert sheet.cell_value(rowx=0, colx=1) == 'First Name'
+
+    print(f'Количество столбцов {sheet.ncols}')
+    print(f'Количество строк {sheet.nrows}')
+    print(f'Пересечение строки 9 и столбца 1 = {sheet.cell_value(rowx=0, colx=1)}')
+    # печать всех строк по очереди
+    for rx in range(sheet.nrows):
+        print(sheet.row(rx))
